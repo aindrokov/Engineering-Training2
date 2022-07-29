429dca068406e6db4ae10c49d2f0fc54b6c40ad7
Artem Indrokov <aindrokov@totalwine.com>
2022-07-29 12:23:49 -0400

    DIG-72911-ReturnJiraInfo

--- app.js
+++ app.js
@@ -3,20 +3,23 @@ const path = require("path");
 const app = express();
 const port = 3000;
 const router = express.Router();
-const dataHandler = require('./dataHandler');
+const dataHandler = require("./dataHandler");
 
-require('dotenv').config();
+require("dotenv").config();
 
 router.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
 router.get("/getJiraTickets", (req, res) => {
-  res.json({"jirasObject":dataHandler.jirasObject});
+  dataHandler.getJiraInfo().then((values) => {
+    console.log(values);
+    res.json({ jirasObject: values });
+  });
 });
 
 app.use("/", router);
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.listen(port, () => {
   console.log(`Engineer Training app listening on port ${port}`);
 });
--- dataHandler.js
+++ dataHandler.js
@@ -33,15 +33,13 @@ async function findJiraIssue(issueNumber) {
   return new Promise(async (resolve, reject) => {
     jira
       .findIssue(issueNumber)
-      //   resolve(issue, titles, links);
-      // })
       .then((issue) => {
-        //console.log("Summary: " + issue.fields.summary);
         const { summary } = issue.fields;
         console.log("Summary: " + summary);
         resolve({
           title: summary,
           link: `https://totalwine.atlassian.net/browse/${issueNumber}`,
+          icon: "bi bi-check-circle-fill",
         });
       })
       .catch((err) => {
@@ -50,32 +48,6 @@ async function findJiraIssue(issueNumber) {
       });
   });
 }
-
-const titles = [
-  "Create and publish a public repository in GitHub under your personal account named 'Engineering Training'",
-  "Create index.html with basic html markup and perform first commit",
-  "Add anchor tags for each completed subtasks",
-  "Make unordered list of anchor tags",
-  "Add the TWM logo as an image to the beginning of the body of the page",
-  "Add style attributes to img tag to setting the height and width of the logo",
-  "Add a head, add a style tag to the head, add a class to style tag, move logo styles to style tag, remove inline styles from logo, add class to the logo",
-  "Add a selector inside style tag that targets the list items and removes the bullet",
-  "Pseudo-selectors - Add hover styling to list elements",
-  "UI Libraries - Add Bootstrap to your page, add check icons to your list, and convert your list into a bootstrap list-group",
-];
-
-const links = [
-  "https://totalwine.atlassian.net/browse/DIG-70749",
-  "https://totalwine.atlassian.net/browse/DIG-70771",
-  "https://totalwine.atlassian.net/browse/DIG-70804",
-  "https://totalwine.atlassian.net/browse/DIG-70805",
-  "https://totalwine.atlassian.net/browse/DIG-70905",
-  "https://totalwine.atlassian.net/browse/DIG-70918",
-  "https://totalwine.atlassian.net/browse/DIG-70993",
-  "https://totalwine.atlassian.net/browse/DIG-71030",
-  "https://totalwine.atlassian.net/browse/DIG-71062",
-  "https://totalwine.atlassian.net/browse/DIG-71085",
-];
 
 let jiraTemplate = {
   icon: "bi bi-check-circle-fill",
@@ -95,24 +67,9 @@ function getIcon() {
 }
 
 class JiraHandler {
-  constructor(links, titles) {
-    this.links = links;
-    this.titles = titles;
+  constructor() {
     this.jirasObject = [];
     this.jiraTicketNumber = [];
-    this.createJiraObject();
-    this.fetchGitHubData();
-    this.getJiraInfo();
-  }
-  createJiraObject() {
-    for (let index = 0; index < this.titles.length; index++) {
-      let icon = getIcon();
-      this.jirasObject.push({
-        title: this.titles[index],
-        link: this.links[index],
-        ...icon,
-      });
-    }
   }
 
   async fetchGitHubData() {
@@ -126,33 +83,35 @@ class JiraHandler {
   }
 
   getJiraInfo() {
-    this.fetchGitHubData().then((listMyCommits) => {
-      let jiraTicketNumber = [];
-      let promises = [];
-      const regex = /([A-Z][A-Z0-9]+-[0-9]+)/g;
-      for (let index = 0; index < listMyCommits.data.length; index++) {
-        let ticketNumber =
-          listMyCommits.data[index].commit.message.match(regex);
-        let indx = jiraTicketNumber.indexOf(ticketNumber);
+    return new Promise(async (resolve) => {
+      this.fetchGitHubData().then((listMyCommits) => {
+        let jiraTicketNumber = [];
+        let promises = [];
+        const regex = /([A-Z][A-Z0-9]+-[0-9]+)/g;
+        for (let index = 0; index < listMyCommits.data.length; index++) {
+          let ticketNumber =
+            listMyCommits.data[index].commit.message.match(regex);
+          let indx = jiraTicketNumber.indexOf(ticketNumber);
 
-        if (ticketNumber !== null && indx === -1) {
-          jiraTicketNumber.push(ticketNumber);
-        } else {
-          console.log(jiraTicketNumber + " Jira ticket duplicates");
+          if (ticketNumber !== null && indx === -1) {
+            jiraTicketNumber.push(ticketNumber);
+          } else {
+            console.log(jiraTicketNumber + " Jira ticket duplicates");
+          }
         }
-      }
-      console.log(jiraTicketNumber);
+        console.log(jiraTicketNumber);
 
-      for (let i = 0; i < jiraTicketNumber.length; i++) {
-        promises.push(findJiraIssue(jiraTicketNumber[i]));
-      }
-      Promise.all(promises).then((values) => {
-        console.log(values);
+        for (let i = 0; i < jiraTicketNumber.length; i++) {
+          promises.push(findJiraIssue(jiraTicketNumber[i]));
+        }
+        Promise.all(promises).then((values) => {
+          resolve(values);
+        });
       });
     });
   }
 }
 
-const jiraHandler = new JiraHandler(links, titles);
+const jiraHandler = new JiraHandler();
 
 module.exports = jiraHandler;

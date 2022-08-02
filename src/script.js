c074d396fcda172b6ea813a78f6719334ddb189f
Artem Indrokov <aindrokov@totalwine.com>
2022-08-02 09:21:38 -0400

    DIG-72965-NewUtilsModule

--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import Button from './components/button';
+import utils from './utils';
 
 ReactDOM.render(
   <Button/>,
@@ -24,37 +25,6 @@ ReactDOM.render(
       });
     });
   }
-
-  const utils = {
-    loadData: async function (callback) {
-      whenCloseClicked();
-      const response = await fetch("/getJiraTickets");
-      const data = await response.json();
-      console.log(data);
-
-      this.renderData(data).then((response) => {
-        list[0].innerHTML = response;
-        return response;
-      });
-      callback();
-
-      let modalContainer = document.getElementById("modal");
-      modalContainer.classList.add("hidden");
-    },
-    renderData: function (data) {
-      return new Promise((resolve) => {
-        let response = "";
-        data.jirasObject.forEach((element) => {
-          let { link, title, icon } = element;
-          response += `<li class="item"><a href= ${link}> 
-        <i class="${icon}">
-        </i> ${title} 
-        </a></li>`;
-        });
-        resolve(response);
-      });
-    },
-  };
 
   function whenCloseClicked() {
     console.log("Clicked Close!");

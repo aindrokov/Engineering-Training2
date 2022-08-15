3373799992ba797d21b61872f2246c0b26dae9c8
Artem Indrokov <aindrokov@totalwine.com>
2022-08-15 08:22:26 -0400

    DIG-73404-StyledComponents

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,5 +1,16 @@
 import React from "react";
 import { connect } from "react-redux";
+import styled from "styled-components";
+
+export const ModalContainer = styled.div`
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  background-color: rgba(0, 0, 0, 0.4);
+`;
 
 function mapStateToProps(state) {
   return {
@@ -8,26 +19,24 @@ function mapStateToProps(state) {
 }
 
 class Modal extends React.Component {
-    constructor(props) {
-      super(props);
-    }
-    render() {
-        if (!this.props.loading) {
-            return;
-          }
-      return (
-        <div id="modal">
-                <div className="background-container">
-                        <div className="popup-container">
-                                <div className="spinner-container">
-                                        <i className="bi bi-arrow-clockwise"></i>
-                                </div>
-                                <span className="loadingtext">Loading...</span>
-                        </div>
-                </div>
-        </div>
-      );
-    }
+  constructor(props) {
+    super(props);
   }
+  render() {
+    if (!this.props.loading) {
+      return;
+    }
+    return (
+      <ModalContainer>
+        <div className="popup-container">
+          <div className="spinner-container">
+            <i className="bi bi-arrow-clockwise"></i>
+          </div>
+          <span className="loadingtext">Loading...</span>
+        </div>
+      </ModalContainer>
+    );
+  }
+}
 
-  export default connect(mapStateToProps)(Modal);
+export default connect(mapStateToProps)(Modal);
